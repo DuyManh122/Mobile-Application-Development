31f230eaa41e0f6dfa41df251687055a3d8464f7
unknown <nguyendangduymanh2003@gmail.com>
2025-03-22 13:32:02 +0700

    Add function to send text from student (MainActivity) to teacher (SecondaryActivity) - MainActivity 	The student enters text into an EditText. 	Submit button 		+ Pass the text inside a Bundle and sent to SecondaryActivity via a Intent. 		+ Start SecondaryActivity with request code is 99. 	Clear button 		+ Clear both student input text and teacher result 		+ Use Toast to show a message about the deletion - SecondaryActivity 	Extracts the text from the Bundle sent by MainActivity 	Display it in EditText to allow teacher modify student's text

--- AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText student_text;
+    private Button student_submit, student_clear;
+    private TextView teacher_text;
+    private void studentSubmitText() {
+        String text = student_text.getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("student_text", text);
+        Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+        intent.putExtra("student_data", bundle);
+        MainActivity.this.startActivityForResult(intent, 99);
+    }
+
+    private void studentClearText() {
+        student_text.setText("");
+        teacher_text.setText("");
+        Toast.makeText(this, "Delete Text Successfully!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        teacher_text = findViewById(R.id.TeacherText);
+        student_text = findViewById(R.id.StudentText);
+        student_submit = findViewById(R.id.StudentSubmit);
+        student_clear = findViewById(R.id.StudentClearText);
+
+
+        student_submit.setOnClickListener(
+                v -> studentSubmitText()
+        );
+        student_clear.setOnClickListener(
+                v -> studentClearText()
+        );
     }
 }
--- AndroidApp/app/src/main/java/com/example/myapplication/SecondaryActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/SecondaryActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondaryActivity extends AppCompatActivity {
-
+    private Intent intent;
+    private EditText student;
+    private String student_text;
+    private Button update_change, restore_student_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_secondary);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.secondary), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        intent = getIntent();
+        Bundle bundle = intent.getBundleExtra("student_data");
+        student_text = bundle.getString("student_text");
+        student = findViewById(R.id.TextNeedToChange);
+        student.setText(student_text);
+
+        update_change = findViewById(R.id.Change);
+        restore_student_text = findViewById(R.id.Restore);
     }
 }
