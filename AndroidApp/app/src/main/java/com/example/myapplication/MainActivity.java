359176b904ba1d18061f24cb39f5948e60505b48
unknown <nguyendangduymanh2003@gmail.com>
2025-04-11 14:29:25 +0700

    Use Firebase Authentication API to authenticate user and password

--- AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TextView Email, Password;
+
+    private Button buttonLogin, buttonCreateNewAccount;
+    private FirebaseAuth mAuth;
+    CheckBox checkboxRemember;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,73 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.Password);
+        buttonLogin = findViewById(R.id.Button_login);
+        buttonCreateNewAccount = findViewById(R.id.Register);
+
+        checkboxRemember = findViewById(R.id.checkbox_remember);
+        sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+
+        String savedEmail = sharedPreferences.getString("email", "");
+        String savedPassword = sharedPreferences.getString("password", "");
+
+        if (!savedEmail.isEmpty()) Email.setText(savedEmail);
+        if (!savedPassword.isEmpty()) Password.setText(savedPassword);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                log();
+            }
+        });
+        buttonCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
     }
+
+    private void log() {
+        String EmailNew = Email.getText().toString().trim();
+        String PasswordNew = Password.getText().toString().trim();
+
+        //Check empty Email and Password
+        if (EmailNew.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Email không được để trống", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (PasswordNew.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Mật khẩu không được để trống", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(EmailNew, PasswordNew)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Đăng nhập thành công với email " + EmailNew,
+                                    Toast.LENGTH_SHORT).show();
+                            if (checkboxRemember.isChecked()) {
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("email", EmailNew);
+                                editor.putString("password", PasswordNew);
+                                editor.apply();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Đăng nhập thất bại",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    void register (){
+        Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+        startActivity(intent);
+    }
+
 }
--- AndroidApp/app/src/main/java/com/example/myapplication/SecondaryActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/SecondaryActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SecondaryActivity extends AppCompatActivity {
+    TextView emailRegister, passwordRegister;
+    Button buttonRegister, buttonLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,61 @@ public class SecondaryActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        emailRegister = findViewById(R.id.Email_register);
+        passwordRegister = findViewById(R.id.Password_register);
+        buttonRegister = findViewById(R.id.Button_register);
+        buttonLogin = findViewById(R.id.Button_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondaryActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+    private void register() {
+        String emailRegisterNew = emailRegister.getText().toString().trim();
+        String passwordRegisterNew = passwordRegister.getText().toString().trim();
+
+        //Check empty Email and Password
+        if (emailRegisterNew.isEmpty()) {
+            Toast.makeText(SecondaryActivity.this, "Email không được để trống", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (passwordRegisterNew.isEmpty()) {
+            Toast.makeText(SecondaryActivity.this, "Mật khẩu không được để trống", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (passwordRegisterNew.length() < 6) {
+            Toast.makeText(this, "Mật khẩu phải có ít nhất 6 ký tự", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(emailRegisterNew, passwordRegisterNew)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SecondaryActivity.this, "Registration successful with email " + emailRegisterNew,
+                                    Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(SecondaryActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(SecondaryActivity.this,
+                                    "Đăng ký thất bại: " + task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
